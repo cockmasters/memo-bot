bfe3a92079791fefcde5c1ba688d87f380dbdd16
huscker <egostalker03@mail.ru>
2023-12-31 15:15:44 +0300

    Added discord bot

--- discord/config.py
+++ discord/config.py
@@ -1,13 +1,7 @@
-from typing import Optional
-
-from pydantic import PostgresDsn
 from pydantic_settings import BaseSettings
 
 
 class BotSettings(BaseSettings):
-    SERVER_PORT: int = 8000
-    SERVER_HOST: str = "127.0.0.1"
-
     BOT_TOKEN: str
 
     class Config:
